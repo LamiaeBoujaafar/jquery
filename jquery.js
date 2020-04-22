c369f301f05191e66ed6923279d1e59fe092317d
lamiae boujaafar <lamiae20ber@gmail.com>
2020-04-22 23:46:42 +0000

    modification1

--- jquery.js
+++ jquery.js
@@ -1,7 +1,19 @@
 $(document).ready(function()
 {
     
-   $("#title").hide();
-  $(".par1").hide();//cacher id   
-            
+    $("#title").click(function()
+        {$(".par2").toggle(1000,function(){
+            $(".par3").toggle(1000); 
+        });
+        });
+    $(".par1").mouseenter(function(){$(".par5").hide(); });
+    $(".par1").mouseleave(function(){$(".par5").show();});
+    $(".par2").hover(function(){
+           $(".par5").hide();
+    },function(){$(".par5").show();});
+    $(".par5").on({
+        mouseenter : function(){$(".par3").hide();},
+        mouseleave : function(){$(".par3").show();}
+    });
+
 });
